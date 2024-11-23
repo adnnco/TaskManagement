025247c3234dcfde3a7f50441cd6e5dec85062a2
Adnan Günay <adnnco@gmail.com>
2024-11-23 12:40:32 +0100

    Task fields created

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
+use Database\Factories\TaskFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
-    use HasFactory;
+    /** @use HasFactory<TaskFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'description', 'status'];
+
+    protected $casts = [
+        'status' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
 }
--- database/migrations/2024_11_23_000631_create_tasks_table.php
+++ database/migrations/2024_11_23_000631_create_tasks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,11 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'completed'])->default('pending');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
