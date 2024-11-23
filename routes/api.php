be3646af8a85ab81807ec233e66f296940658f1b
Adnan Günay <adnnco@gmail.com>
2024-11-23 14:44:42 +0100

    Task Controller created

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->group(function () {
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('logout', [AuthController::class, 'logout']);
 });
+
+Route::apiResource('tasks', TaskController::class)->middleware('auth:api')->except('create', 'edit');
